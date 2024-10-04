26fd702cbb8e647a89bbce68be0163e4c5c871b8
lord <konadulordkweku@gmail.com>
2024-10-04 13:17:14 +0000

    Refactor app.py to handle task updates via API

--- Tasks/task_1/middleware/app.py
+++ Tasks/task_1/middleware/app.py
@@ -1,15 +1,13 @@
-from flask import Flask
-from middleware.db import (
-    get_db_connection,
-    update_task,
-)
+from flask import Flask, abort, jsonify
+from middleware.db import update_task
 
 app = Flask(__name__)
 
 
 @app.route("/api/update/<int:id>/", strict_slashes=True)
 def update(id):
-
+    if not id:
+        return jsonify({"error": "id is required"}), 400
     cursor = update_task(id)
 
 
