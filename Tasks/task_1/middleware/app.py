ad6e7cde3274b3b9895ce8a81985940b22ab1bd7
Blaq <eagyemang048@st.ug.edu.gh>
2024-10-04 13:29:21 +0000

    merge with blaq

--- Tasks/task_1/middleware/app.py
+++ Tasks/task_1/middleware/app.py
@@ -13,16 +13,3 @@ def update(id):
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
-
-
-
-### get function
-
-def get_task_title(task_id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("CALL get_task_title(%s);", (task_id,))
-    title = cursor.fetchone()[0]  # Fetch the output
-    cursor.close()
-    conn.close()
-    return title
