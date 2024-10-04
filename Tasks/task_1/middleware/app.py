936051cc90a9d461e3249a16c54812f0c054e459
Blaq <eagyemang048@st.ug.edu.gh>
2024-10-04 13:19:30 +0000

    get

--- Tasks/task_1/middleware/app.py
+++ Tasks/task_1/middleware/app.py
@@ -1,17 +1,20 @@
 from flask import Flask
-from middleware.db import (
-    get_db_connection,
-    update_task,
-)
 
 app = Flask(__name__)
 
 
-@app.route("/api/update/<int:id>/", strict_slashes=True)
-def update(id):
-
-    cursor = update_task(id)
-
-
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
+
+
+
+### get function
+
+def get_task_title(task_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("CALL get_task_title(%s);", (task_id,))
+    title = cursor.fetchone()[0]  # Fetch the output
+    cursor.close()
+    conn.close()
+    return title
