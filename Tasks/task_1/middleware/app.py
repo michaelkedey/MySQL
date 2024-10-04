850fd33352552c1a0b124573e7b2ba0c1a14de8a
lord <konadulordkweku@gmail.com>
2024-10-04 12:49:38 +0000

    Refactor middleware to handle task updates via API

--- Tasks/task_1/middleware/app.py
+++ Tasks/task_1/middleware/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from middleware.db import (
+    get_db_connection,
+    update_task,
+)
 
 app = Flask(__name__)
+
+
+@app.route("/api/update/<int:id>/", strict_slashes=True)
+def update(id):
+
+    cursor = update_task(id)
 
 
 if __name__ == "__main__":
--- Tasks/task_1/middleware/db.py
+++ Tasks/task_1/middleware/db.py
@@ -13,30 +13,6 @@ def get_db_connection():
     return conn
 
 
-# Function to fetch all tasks
-def fetch_all_tasks():
-    conn = get_db_connection()
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    cursor.execute("SELECT * FROM tasks ORDER BY id;")
-    tasks = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return tasks
-
-
-# Function to insert a task
-def insert_task(title, description):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO tasks (title, description) VALUES (%s, %s);", (title, description)
-    )
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-
-# Function to update a task
 def update_task(task_id, title, description, done):
     conn = get_db_connection()
     cursor = conn.cursor()
@@ -47,24 +23,3 @@ def update_task(task_id, title, description, done):
     conn.commit()
     cursor.close()
     conn.close()
-
-
-# Function to delete a task
-def delete_task(task_id):
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("DELETE FROM tasks WHERE id = %s;", (task_id,))
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-
-# Function to get a single task
-def get_task_by_id(task_id):
-    conn = get_db_connection()
-    cursor = conn.cursor(cursor_factory=RealDictCursor)
-    cursor.execute("SELECT * FROM tasks WHERE id = %s;", (task_id,))
-    task = cursor.fetchone()
-    cursor.close()
-    conn.close()
-    return task
